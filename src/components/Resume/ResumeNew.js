5564f675feff82ff3f31eae50c0773675860269b
Sahil Sudhir <sudhirsahil2001@gmail.com>
2023-05-22 20:20:19 -0400

    updated resume links

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/pantharex/pantharex/master/Sahil_Sudhir_Resume.pdf";
+  "https://raw.githubusercontent.com/pantharex/Personal-Website-2.0/master/src/Assets/sahil_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
